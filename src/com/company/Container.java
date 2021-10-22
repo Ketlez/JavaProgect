aeecce0d701b6d21959f3be4e4ac8ae066f8a7bc
Ketlez <dimaponomorev01@mail.ru>
2021-10-23 02:36:54 +0300

    Initial commit

---
+++ src/com/company/Container.java
@@ -0,0 +1,87 @@
+package com.company;
+
+public class Container {
+    private int [] arr;
+    private int size;
+    private int currentSize;
+    private int increaseSize;
+
+    Container()
+    {
+        size = increaseSize = 7;
+        currentSize = 0;
+        arr = new int[size];
+    }
+
+    private void increaseSize ()
+    {
+        size += increaseSize;
+        int [] new_arr = new int[size];
+        for (int i = 0; i < size - increaseSize; i++)
+        {
+            new_arr[i]=arr[i];
+        }
+        arr = new_arr;
+    }
+
+    private void decreaseSize ()
+    {
+        size -= increaseSize;
+        int [] new_arr = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            new_arr[i]=arr[i];
+        }
+        arr = new_arr;
+    }
+
+    public void add_item (int elem)
+    {
+        if(currentSize == size)
+            increaseSize();
+        arr[currentSize]=elem;
+        currentSize++;
+    }
+
+    public void remove_item (int tm)
+    {
+        --tm;
+
+        if(tm>=0 && tm<currentSize)
+        {
+            currentSize--;
+            int [] new_arr = new int[size];
+            int i = 0, j =0;
+            while (i!=currentSize)
+            {
+                if(j!=tm)
+                {
+                    new_arr[i] = arr[j];
+                    i++;
+                }
+                j++;
+            }
+            arr = new_arr;
+            if(currentSize<=size-increaseSize) decreaseSize();
+        }
+        else System.out.println("Item number does not exist!");
+    }
+
+    public int get_i_Elem (int i)
+    {
+        --i;
+        int elem =0;
+        if(i<0||i>=currentSize)
+            System.out.println("Element doesn't exist!");
+        elem = arr[i];
+        return elem;
+    }
+    public int getCurrentSize ()
+    {
+        return currentSize;
+    }
+    public int getSize ()
+    {
+        return size;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main (String[]args)
+    {
+        Container arr = new Container();
+        System.out.println("Create a 7-item container: ");
+        for(int i=0; i<9; i++)
+        {
+            arr.add_item(i);
+            System.out.println("Size:" + arr.getSize()+ " Elem №" +arr.getCurrentSize()+": "+ arr.get_i_Elem(i+1));
+        }
+
+        System.out.println("Remove 3 items: ");
+        arr.remove_item(1);
+        arr.remove_item(1);
+        arr.remove_item(1);
+        for(int i=0; i<arr.getCurrentSize(); i++) {
+            System.out.println("Size:" + arr.getSize() + " Elem №" + (i+1) + ": " + arr.get_i_Elem(i + 1));
+        }
+        System.out.println("Current item: №" + arr.getCurrentSize()+ ", Container size:" + arr.getSize());
+        System.out.println("Current item: №4 = "+ arr.get_i_Elem(4));
+    }
+}
